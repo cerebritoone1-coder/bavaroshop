1e02cb4ca4c5e64e37692fb31f529252eb85a351
BavaroShop Dev <dev@bavaroshop.do>
2026-02-22 13:31:42 -0400

    Fix AppConfig - remove ready()

--- apps/store/apps.py
+++ apps/store/apps.py
@@ -1,19 +1,6 @@
 from django.apps import AppConfig
-import os
+
 
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.store'
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-        if username and email and password:
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(username, email, password)
