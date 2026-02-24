be17ae24b44c51b811430c6d051a86d0de73d952
BavaroShop Dev <dev@bavaroshop.do>
2026-02-24 13:57:04 -0400

    Secure admin by IP

---
+++ config/middleware.py
@@ -0,0 +1,25 @@
+import os
+from django.http import HttpResponseForbidden
+
+
+class AdminIPRestrictionMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.allowed_ip = os.getenv("ALLOWED_ADMIN_IP")
+
+    def __call__(self, request):
+
+        if request.path.startswith("/admin"):
+
+            user_ip = request.META.get("HTTP_X_FORWARDED_FOR")
+
+            if user_ip:
+                user_ip = user_ip.split(",")[0]
+            else:
+                user_ip = request.META.get("REMOTE_ADDR")
+
+            if self.allowed_ip and user_ip != self.allowed_ip:
+                return HttpResponseForbidden("Admin access denied")
+
+        return self.get_response(request)
--- config/urls.py
+++ config/urls.py
@@ -1,41 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-# 🔥 RESET ADMIN TEMPORAL
-def reset_admin(request):
-    if request.GET.get("key") != "2026":
-        return HttpResponse("Forbidden", status=403)
-
-    User = get_user_model()
-
-    user, created = User.objects.get_or_create(
-        username="admin",
-        defaults={"email": "cerebritoone1@gmail.com"}
-    )
-
-    user.set_password("marino19870603")
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
-
-    return HttpResponse("ADMIN RESETEADO OK")
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.store.urls')),
-
-    # 🔥 RUTA SECRETA
-
-    
-    path('reset-admin-2026/', reset_admin),
 ]
 
-# 👇 MEDIA SOLO EN DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
